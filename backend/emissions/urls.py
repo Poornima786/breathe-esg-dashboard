7dbc8d1103345f60bc36a93fb29d12ef9ea0eb91
Poornima <poornimas019@gmail.com>
2026-05-26 22:24:22 +0530

    Added gunicorn and requirements

--- backend/emissions/urls.py
+++ backend/emissions/urls.py
@@ -1,6 +1,10 @@
+from .views import upload_csv
 from django.urls import path
-from .views import get_records
+from .views import get_records, update_status
 
 urlpatterns = [
     path('records/', get_records),
+    path("upload/", upload_csv),
+    path('records/<int:pk>/update/', update_status),
 ]
+
--- backend/emissions/views.py
+++ backend/emissions/views.py
@@ -1,7 +1,9 @@
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import EmissionRecord
 from .serializers import EmissionRecordSerializer
+import csv
+from io import TextIOWrapper
 
 
 @api_view(['GET'])
@@ -9,3 +11,39 @@ def get_records(request):
     records = EmissionRecord.objects.all()
     serializer = EmissionRecordSerializer(records, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def update_status(request, pk):
+    try:
+        record = EmissionRecord.objects.get(id=pk)
+
+        status = request.data.get('status')
+        record.status = status
+        record.save()
+
+        return Response({"message": "Status updated"})
+
+    except EmissionRecord.DoesNotExist:
+        return Response({"error": "Record not found"})
+
+
+@api_view(['POST'])
+def upload_csv(request):
+    file = request.FILES.get('file')
+
+    if not file:
+        return Response({"error": "No file uploaded"})
+
+    decoded_file = TextIOWrapper(file, encoding='utf-8')
+    reader = csv.DictReader(decoded_file)
+
+    for row in reader:
+        EmissionRecord.objects.create(
+            source=row['source'],
+            value=row['value'],
+            unit=row['unit'],
+            status=row['status']
+        )
+
+    return Response({"message": "CSV uploaded successfully"})
